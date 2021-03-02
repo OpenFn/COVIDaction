5c7c7134f5bea1a4308a8c23350d5ab074c19539
Aissatou Diallo <aicha328@gmail.com>
2021-03-02 14:48:13 -0500

    aissatou@openfn.org's change on OpenFn.org

--- salesforce.js
+++ salesforce.js
@@ -1,5 +1,5 @@
 create('Contact', {
-   //Name: dataValue("body.Patient_name"),
+   Name: dataValue("body.Patient_name"),
    Age__c: dataValue("body.Age"),
    Sex__c: dataValue("body.Sex"),	
    Case_ID__c: dataValue("body.National_ID"),	
