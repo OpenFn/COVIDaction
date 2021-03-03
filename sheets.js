be4019fd680bbad7b782f76471702ca33c5b8c7c
Aissatou Diallo <aicha328@gmail.com>
2021-03-02 23:54:33 -0500

    aissatou@openfn.org's change on OpenFn.org

--- sheets.js
+++ sheets.js
@@ -1,1 +1,13 @@
-// Your job goes here.
+appendValues({
+  spreadsheetId: '1EFkY4zD4qqxnJdH-QaeasKd1zXC-1sNKpEg08W-3sT0',
+  range: 'COVID-19 Cases!A2',
+  values: (states)=> {
+    console.log('Submission data: ' + JSON.stringify(state.data, null, 2));
+    const kobo = state.data.body;
+    return [
+      [
+        kobo['Patient_name'],
+        ],
+      ];
+  },
+});
