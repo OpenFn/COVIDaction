ca7e9a6d3b31eb50f21cab51ef32bde8a008c60d
Taylor Downs <taylor@openfn.org>
2021-03-22 13:17:33 +0000

    taylor@openfn.org's change on OpenFn.org

---
+++ dhis2.js
@@ -0,0 +1,32 @@
+// See list of cases here: https://covid19.dhis2.org/demo/dhis-web-tracker-capture/index.html#/?program=DM9n1bUw8W8
+createTEI({
+  orgUnit: 'GD7TowwI46c',
+  trackedEntityType: 'MCPQUTHX1Ze',
+  attributes: [
+    {
+      attribute: 'he05i8FUwu3', //case id
+      value: dataValue('body._id'),
+    },
+    {
+      attribute: 'sB1IHYu2xQT', //first name
+      value: dataValue('body.Patient_name'),
+    },
+    {
+      attribute: 'ENRjVGxVL6l', //last name
+      value: dataValue('body.Patient_name'),
+    },
+    {
+      attribute: 'Rv8WM2mTuS5', //age
+      value: dataValue('body.Age'),
+    },
+  ],
+  enrollments: [
+    {
+      orgUnit: 'GD7TowwI46c',
+      program: 'DM9n1bUw8W8',
+      programState: 'sAV9jAajr8x',
+      enrollmentDate: dataValue('body.Date'),
+      incidentDate: dataValue('body.Covid_19_suspected_criteria/Speciman_Collection_date')
+    },
+  ],
+});
