b591ee18543844e257473ca2aec7870bc0a4c7bc
Taylor Downs <taylor@openfn.org>
2021-04-08 15:31:13 +0100

    taylor@openfn.org's change on OpenFn.org

--- salesforce.js
+++ salesforce.js
@@ -1,6 +1,6 @@
 create('Contact', {
   FirstName: dataValue("body.Patient_name"),
-  LastName: dataValue("body.Last_Name_of_Patient"),
+  LastName: dataValue('body.Last_Name_of_Patient'),
   Age__c: dataValue("body.Age"),
   Sex__c: dataValue("body.Sex"),	
   Case_ID__c: dataValue("body.National_ID"),	
