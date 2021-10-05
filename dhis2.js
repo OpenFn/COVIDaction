9ed0f4b440f3f59229a40b3fa5c2d56e8d636e3e
Taylor Downs <taylor@openfn.org>
2021-10-05 11:19:54 +0100

    taylor@openfn.org's change on OpenFn.org

--- dhis2.js
+++ dhis2.js
@@ -1,7 +1,6 @@
-// See list of cases here: https://covid19.dhis2.org/demo/dhis-web-tracker-capture/index.html#/?program=DM9n1bUw8W8
 createTEI({
-  orgUnit: 'GD7TowwI46c',
-  trackedEntityType: 'MCPQUTHX1Ze',
+  orgUnit: 'DiszpKrYNg8',
+  trackedEntityType: 'nEenWmSyUEp',
   attributes: [
     {
       attribute: 'he05i8FUwu3', // case id
@@ -18,15 +17,48 @@ createTEI({
     {
       attribute: 'Rv8WM2mTuS5', // age
       value: dataValue('body.Age'),
-    },
+    },  
   ],
   enrollments: [
     {
-      orgUnit: 'GD7TowwI46c',
-      program: 'DM9n1bUw8W8',
-      programState: 'sAV9jAajr8x',
+      orgUnit: 'DiszpKrYNg8',
+      program: 'uy2gU8kT1jF',
+      programState: 'oRySG82BKE6', // active
       enrollmentDate: dataValue('body.Date'),
-      incidentDate: dataValue('body.Covid_19_suspected_criteria/Speciman_Collection_date')
+      incidentDate: dataValue('body.Date'),
     },
   ],
 });
+
+// // See list of cases here: https://covid19.dhis2.org/demo/dhis-web-tracker-capture/index.html#/?program=DM9n1bUw8W8
+// createTEI({
+//   orgUnit: 'GD7TowwI46c',
+//   trackedEntityType: 'MCPQUTHX1Ze',
+//   attributes: [
+//     {
+//       attribute: 'he05i8FUwu3', // case id
+//       value: dataValue('body._id'),
+//     },
+//     {
+//       attribute: 'sB1IHYu2xQT', // first name
+//       value: dataValue('body.Patient_name'),
+//     },
+//     {
+//       attribute: 'ENRjVGxVL6l', // last name
+//       value: dataValue('body.Last_Name_of_Patient'),
+//     },
+//     {
+//       attribute: 'Rv8WM2mTuS5', // age
+//       value: dataValue('body.Age'),
+//     },
+//   ],
+//   enrollments: [
+//     {
+//       orgUnit: 'GD7TowwI46c',
+//       program: 'DM9n1bUw8W8',
+//       programState: 'sAV9jAajr8x',
+//       enrollmentDate: dataValue('body.Date'),
+//       incidentDate: dataValue('body.Covid_19_suspected_criteria/Speciman_Collection_date')
+//     },
+//   ],
+// });
