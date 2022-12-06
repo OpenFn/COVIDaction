f745de9866dfc186d0955585d926a2ae68db73a9
Aleksa Krolls <aleksagk@gmail.com>
2022-12-06 13:26:46 +0200

    aleksa@openfn.org's change on OpenFn.org

--- dhis2.js
+++ dhis2.js
@@ -1,18 +1,39 @@
-createTEI({
+upsertTEI('vwHCYhHP0eL', {
   orgUnit: 'DiszpKrYNg8',
   trackedEntityType: 'nEenWmSyUEp',
-  program: 'uy2gU8kT1jF',
-  attributes: [
-    { attribute: 'zDhUuAYrxNC', value: dataValue('body.Patient_name') },
-    { attribute: 'w75KJ2mc4zz', value: dataValue('body.Last_Name_of_Patient') },
-  ],
-  // enrollments: [
+  // program: 'uy2gU8kT1jF',
+  // attributes: [
   //   {
-  //     orgUnit: 'DiszpKrYNg8',
-  //     program: 'uy2gU8kT1jF',
-  //     programState: 'oRySG82BKE6', // active
-  //     enrollmentDate: dataValue('body.Date'),
-  //     incidentDate: dataValue('body.Date'),
+  //     attribute: 'lZGmxYbs97q',
+  //     value: '77790012',
+  //   },
+  //   {
+  //     attribute: 'w75KJ2mc4zz',
+  //     value: 'Gigiwe',
+  //   },
+  //   {
+  //     attribute: 'zDhUuAYrxNC',
+  //     value: 'Mwanza',
   //   },
   // ],
 });
+
+
+// createTEI({
+//   orgUnit: 'DiszpKrYNg8',
+//   trackedEntityType: 'nEenWmSyUEp',
+//   program: 'uy2gU8kT1jF',
+//   attributes: [
+//     { attribute: 'zDhUuAYrxNC', value: dataValue('body.Patient_name') },
+//     { attribute: 'w75KJ2mc4zz', value: dataValue('body.Last_Name_of_Patient') },
+//   ],
+//   // enrollments: [
+//   //   {
+//   //     orgUnit: 'DiszpKrYNg8',
+//   //     program: 'uy2gU8kT1jF',
+//   //     programState: 'oRySG82BKE6', // active
+//   //     enrollmentDate: dataValue('body.Date'),
+//   //     incidentDate: dataValue('body.Date'),
+//   //   },
+//   // ],
+// });
