67b1a113112187baadfa4182f958826f0e36b005
Aleksa Krolls <aleksagk@gmail.com>
2022-12-06 13:21:50 +0200

    aleksa@openfn.org's change on OpenFn.org

--- dhis2.js
+++ dhis2.js
@@ -6,13 +6,13 @@ createTEI({
     { attribute: 'zDhUuAYrxNC', value: dataValue('body.Patient_name') },
     { attribute: 'w75KJ2mc4zz', value: dataValue('body.Last_Name_of_Patient') },
   ],
-  // enrollments: [
-  //   {
-  //     orgUnit: 'DiszpKrYNg8',
-  //     program: 'uy2gU8kT1jF',
-  //     programState: 'oRySG82BKE6', // active
-  //     enrollmentDate: dataValue('body.Date'),
-  //     incidentDate: dataValue('body.Date'),
-  //   },
-  // ],
+  enrollments: [
+    {
+      orgUnit: 'DiszpKrYNg8',
+      program: 'uy2gU8kT1jF',
+      programState: 'oRySG82BKE6', // active
+      enrollmentDate: dataValue('body.Date'),
+      incidentDate: dataValue('body.Date'),
+    },
+  ],
 });
