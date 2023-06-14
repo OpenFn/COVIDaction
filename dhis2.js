0031296264ff56704790264d0024ea565d5cb7f9
Taylor Downs <taylor@openfn.org>
2023-06-14 13:25:17 +0100

    taylor@openfn.org's change on OpenFn.org

--- dhis2.js
+++ dhis2.js
@@ -1,7 +1,7 @@
 create('trackedEntityInstances', {
   orgUnit: 'g8upMTyEZGZ',
   trackedEntityType: 'nEenWmSyUEp',
-  // program: '',
+  program: 'IpHINAT79UW',
   attributes: [
     { attribute: 'zDhUuAYrxNC', value: dataValue('body.Patient_name') },
     { attribute: 'w75KJ2mc4zz', value: dataValue('body.Last_Name_of_Patient') },
